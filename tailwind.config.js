fee9f845f9701604f97d5c052b2429c96b8d6351
Israel Castro <israel.castro@gmail.com>
2020-08-01 15:27:50 -0500

    [CONFIG] Colors, dimensions and typografy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+    }
   },
   variants: {},
   plugins: [],
